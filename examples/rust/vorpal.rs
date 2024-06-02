5aa945133c5739e0cb5a8a1d8aaf233a2a92cb95
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-06-02 01:51:30 -0700

    feat: add new dependencies and refactor package handling
    - Added new dependencies: `bstr`, `crossbeam-deque`, `crossbeam-epoch`, `crossbeam-utils`, `globset`, `globwalk`, `ignore`, `pest`, `pest_derive`, `pest_generator`, `pest_meta`, `ryu`, `serde_json`, `signal-hook-registry`, `tera`, `ucd-trie`, `unic-char-property`, `unic-char-range`, `unic-common`, `unic-segment`, `unic-ucd-segment`, `unic-ucd-version`, `tempfile`.
    - Updated `proc-macro2` dependency version.
    - Refactored `PackageArgs` struct to use string slices for better memory management.
    - Modified `run` function to accept a reference to `Package` instead of consuming it.
    - Updated `prepare` and `build` functions to work with references and return appropriate types.
    - Improved logging and error handling in package preparation and building processes.

--- examples/rust/vorpal.rs
+++ examples/rust/vorpal.rs
@@ -6,20 +6,16 @@ pub async fn main() -> Result<(), anyhow::Error> {
     let example = package::new(package::PackageArgs {
         build_phase: r#"
             echo "hello, world!" >> example.txt
-        "#
-        .to_string(),
-        ignore_paths: vec![
-            ".direnv".to_string(),
-            ".git".to_string(),
-            "target".to_string(),
-        ],
+            cat example.txt
+        "#,
+        ignore_paths: vec![".direnv", ".git", "target"],
         install_phase: r#"
-            cp example.txt $output
-        "#
-        .to_string(),
-        name: "example".to_string(),
-        source: ".".to_string(),
+            mkdir -p $OUTPUT
+            cp example.txt $OUTPUT/example.txt
+        "#,
+        name: "example",
+        source: ".",
     });
 
-    package::run(example).await
+    package::run(&example).await
 }
--- rust/src/package/mod.rs
+++ rust/src/package/mod.rs
@@ -9,37 +9,37 @@ use vorpal::api::{BuildRequest, Package, PrepareRequest};
 use vorpal::notary;
 use vorpal::store;
 
-pub struct PackageArgs {
-    pub build_phase: String,
-    pub ignore_paths: Vec<String>,
-    pub install_phase: String,
-    pub name: String,
-    pub source: String,
+pub struct PackageArgs<'a> {
+    pub build_phase: &'a str,
+    pub ignore_paths: Vec<&'a str>,
+    pub install_phase: &'a str,
+    pub name: &'a str,
+    pub source: &'a str,
 }
 
 pub fn new(args: PackageArgs) -> Package {
     Package {
-        build_phase: args.build_phase,
-        ignore_paths: args.ignore_paths,
-        install_phase: args.install_phase,
-        name: args.name,
-        source: args.source,
+        build_phase: args.build_phase.to_string(),
+        ignore_paths: args.ignore_paths.iter().map(|i| i.to_string()).collect(),
+        install_phase: args.install_phase.to_string(),
+        name: args.name.to_string(),
+        source: args.source.to_string(),
     }
 }
 
-pub async fn run(package: Package) -> Result<(), anyhow::Error> {
-    let package_dir = store::get_package_dir();
+pub async fn run(package: &Package) -> Result<(), anyhow::Error> {
+    let package_dir = store::get_package_path();
     if !package_dir.exists() {
         fs::create_dir_all(&package_dir)?;
     }
 
-    println!("Preparing: {:?}", package.name);
+    println!("Preparing: {}", package.name);
 
-    let source_id = prepare(package_dir, package.clone()).await?;
+    let source_id = prepare(package_dir, package).await?;
 
-    println!("Building: {:?}-{:?}", package.name, source_id);
+    println!("Building: {}-{}", package.name, source_id);
 
-    let _ = build(source_id, package.clone()).await?;
+    let _ = build(source_id, package).await?;
 
     // TODO: build method
 
@@ -50,28 +50,28 @@ pub async fn run(package: Package) -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-async fn prepare(package_dir: PathBuf, package: Package) -> Result<String, anyhow::Error> {
+async fn prepare(package_dir: PathBuf, package: &Package) -> Result<i32, anyhow::Error> {
     let source = Path::new(&package.source).canonicalize()?;
     let source_ignore_paths = package
         .ignore_paths
         .iter()
         .map(|i| Path::new(i).to_path_buf())
-        .collect();
-    let source_files = store::get_file_paths(&source, source_ignore_paths)?;
-    let source_files_hashes = store::get_file_hashes(source_files.clone())?;
-    let source_hash = store::get_source_hash(source_files_hashes.clone())?;
+        .collect::<Vec<PathBuf>>();
+    let source_files = store::get_file_paths(&source, &source_ignore_paths)?;
+    let source_files_hashes = store::get_file_hashes(&source_files)?;
+    let source_hash = store::get_source_hash(&source_files_hashes)?;
     let source_dir_name = store::get_package_dir_name(&package.name, &source_hash);
     let source_dir = package_dir.join(&source_dir_name).with_extension("source");
     let source_tar = source_dir.with_extension("source.tar.gz");
     if !source_tar.exists() {
         println!("Creating source tar: {:?}", source_tar);
-        store::compress_files(source.clone(), source_tar.clone(), source_files.clone())?;
+        store::compress_files(&source, &source_tar, &source_files)?;
         fs::set_permissions(&source_tar, fs::Permissions::from_mode(0o444))?;
     }
 
-    println!("Source file: {:?}", source_tar);
+    println!("Source file: {}", source_tar.display());
 
-    let private_key = match notary::get_private_key() {
+    let private_key = match notary::get_private_key().await {
         Ok(key) => key,
         Err(e) => anyhow::bail!("Failed to get private key: {:?}", e),
     };
@@ -80,12 +80,12 @@ async fn prepare(package_dir: PathBuf, package: Package) -> Result<String, anyho
     let source_data = fs::read(&source_tar)?;
     let source_signature = signing_key.sign_with_rng(&mut signing_rng, &source_data);
 
-    println!("Source signature: {:?}", source_signature);
+    println!("Source signature: {}", source_signature.to_string());
 
     let request = tonic::Request::new(PrepareRequest {
         source_data,
         source_hash,
-        source_name: package.name,
+        source_name: package.name.to_string(),
         source_signature: source_signature.to_string(),
     });
     let mut client = PackageServiceClient::connect("http://[::1]:15323").await?;
@@ -95,15 +95,15 @@ async fn prepare(package_dir: PathBuf, package: Package) -> Result<String, anyho
     Ok(response_data.source_id)
 }
 
-async fn build(package_id: String, package: Package) -> Result<Vec<u8>, anyhow::Error> {
+async fn build(package_id: i32, package: &Package) -> Result<Vec<u8>, anyhow::Error> {
     let request = tonic::Request::new(BuildRequest {
-        build_phase: package.build_phase,
-        install_phase: package.install_phase,
-        source_id: package_id.clone(),
+        build_phase: package.build_phase.to_string(),
+        install_phase: package.install_phase.to_string(),
+        source_id: package_id,
     });
     let mut client = PackageServiceClient::connect("http://[::1]:15323").await?;
     let response = client.build(request).await?;
     let response_data = response.into_inner();
 
-    Ok(response_data.data)
+    Ok(response_data.package_data)
 }
