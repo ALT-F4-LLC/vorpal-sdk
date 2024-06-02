db3fe3db4f21f849657bec372f8d261da7bf0da4
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-06-02 03:59:58 -0700

    feat: add example-rust profile and update paths
    - Added `example-rust` profile to GitHub Actions workflow.
    - Renamed `examples/rust` directory to `example/rust`.
    - Updated `Cargo.toml` to use the `vorpal-sdk` from the git repository.
    - Added `example-rust` package to `flake.nix` with necessary configurations.

--- flake.nix
+++ flake.nix
@@ -16,6 +16,20 @@
         inherit (rustPlatform) buildRustPackage;
       in {
         packages = {
+          example-rust = buildRustPackage {
+            cargoLock = {
+              lockFile = ./example/rust/Cargo.lock;
+              outputHashes = {
+                "vorpal-0.1.0" = "sha256-yps0MqnBtA1xF+0ci8V7GDFxvoxfd3ufKMlHkW9bQSM=";
+                "vorpal-sdk-0.1.0" = "sha256-U/8lEM2B7MGCckams+uz0uozv4Yq499gVoaIAK8jV7o=";
+              };
+            };
+            nativeBuildInputs = [protobuf];
+            pname = "example-rust";
+            src = ./example/rust;
+            version = "0.1.0";
+          };
+
           sdk-rust = buildRustPackage {
             cargoLock = {
               lockFile = ./rust/Cargo.lock;
